ca32f4f1dc2d01c2d537b8340a609a52f344b25c
anton_podolsky <antonp@wix.com>
2017-09-24 16:36:01 +0300

    Selector: basic dynamic storybook

--- stories/Selector/ExampleControlled.js
+++ stories/Selector/ExampleControlled.js
@@ -1,46 +1,53 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import ModalSelector from '../../src/ModalSelector';
 import Selector from '../../src/Selector';
-import {Button} from 'wix-style-react/Backoffice';
+import Label from '../../src/Label';
+import Input from '../../src/Input';
+import styles from './ExampleStandard.scss';
 
 class ControlledSelector extends Component {
 
   constructor({isOpen = false}) {
     super();
+
     this.state = {
-      isOpen,
-      footerChecked: false
+      id: 1,
+      title: 'Title',
+      selected: false
     };
+  }
+
+  setComponentState(name, value) {
+    this.setState(prevState => ({...prevState, ...{[name]: value}}));
   }
 
   render() {
     return (
       <div>
-
         <form className={styles.form}>
           <div className={styles.input}>
             <div className={styles.option}>
-              <Label>Title</Label>>
+              <Label>Title</Label>
               <div className={styles.flex}>
                 <Input
                   size="small"
-                  value={this.state.label.children}
-                  onChange={e => this.setComponentState('label', {children: e.target.value})}
-                  />
+                  value={this.state.title}
+                  onChange={e => this.setComponentState('title', e.target.value)}
+                />
               </div>
             </div>
           </div>
         </form>
+
         <Selector
-          title="TITLE TEXT"
-          id={i}
-          key={i}
+          title={this.state.title}
+          id={this.state.id}
+          key="1"
           subTitle="SUBTITLE TEXT"
           imageSrc="http://media.istockphoto.com/photos/orange-picture-id185284489?k=6&m=185284489&s=612x612&w=0&h=x_w4oMnanMTQ5KtSNjSNDdiVaSrlxM4om-3PQTIzFaY="
           imageSize="Cinema View"
+          isSelected={this.state.selected}
           onToggle={this.selectorToggle}
-          isSelected={x.selected}
           >
           {/* <Selector.ExtraText text="Extra Text"/> */}
           {/* <Selector.ExtraIcon name="add"/> */}
